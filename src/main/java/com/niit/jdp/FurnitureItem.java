8f1abdad714de85076b96b3ad165f895aa31be63
M V K <krishna.mugala9@gmail.com>
2022-08-24 14:50:51 +0530

    added constructor in the class

--- src/main/java/com/niit/jdp/FurnitureItem.java
+++ src/main/java/com/niit/jdp/FurnitureItem.java
@@ -16,3 +16,16 @@ public class FurnitureItem {
     String furnitureUsage;
     double furniturePrice;
 
+
+    public furnitureItem(double furniturePrice, double furnitureCode, String furnitureColour, String furnitureGrade, String furnitureType, String furnitureUsage) {
+        this.furniturePrice = furniturePrice;
+        System.out.println(this.furniturePrice);
+        this.furnitureCode = furnitureCode;
+        this.furnitureColour = furnitureColour;
+        this.furnitureGrade = furnitureGrade;
+        this.furnitureType = furnitureType;
+        this.furnitureUsage = furnitureUsage;
+    }
+
+
+
